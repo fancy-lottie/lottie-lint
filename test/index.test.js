94f9c83a8041302fc8174209921720a176f07473
天辙 <weiesky.wangc@alibaba-inc.com>
2019-06-12 19:03:25 +0800

    feat: lottie-lint的结构定义优化 以及 文本内容优化

--- test/index.test.js
+++ test/index.test.js
@@ -5,27 +5,283 @@ const assert = require('assert');
 const linter = require('../src/index');
 
 describe('linter', function() {
-  it('matte 遮罩层检测', () => {
+  it('检测样本一', () => {
     const matteJSON = require('./case_data/matte.json');
     const reports = linter(matteJSON).reports;
 
-    assert.deepStrictEqual(reports, [{
-      message: '兼容性警告⚠️: 当前图层存在遮罩层，建议使用蒙版替代',
-      name: '发射条.png',
-      path: '#/assets[7]/layers[2]',
-      rule: 'incompatible_matte_not_suggested',
-    }]);
+    assert.deepStrictEqual(reports, [
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: '形状图层 1',
+        element: {
+          asset: 7,
+          layer: 1,
+        },
+      },
+      {
+        message: '图层存在 “遮罩层” 特性，极其损耗性能，建议不使用，或用 “蒙版” 替代',
+        type: 'warn',
+        rule: 'warn_matte_not_suggested',
+        name: '发射条.png',
+        element: {
+          asset: 7,
+          layer: 2,
+        },
+      },
+    ]);
   });
 
-  it('Layer Effects 检测', () => {
+  it('检测样本二', () => {
     const matteJSON = require('./case_data/layerEffects.json');
     const reports = linter(matteJSON).reports;
 
-    assert.deepStrictEqual(reports, [{
-      message: '兼容性警告⚠️: 当前图层存在 Layer Effects，在 iOS 和 Android 上不支持',
-      rule: 'incompatible_layer_effects',
-      name: 'shadow',
-      path: '#/layers[11]',
-    }]);
+    assert.deepStrictEqual(reports, [
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A4',
+        element: {
+          asset: -1,
+          layer: 1,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'girlcryfont',
+        element: {
+          asset: -1,
+          layer: 4,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 4,
+          shape: 0,
+          groupIt: 3,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 4,
+          shape: 1,
+          groupIt: 7,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 4,
+          shape: 2,
+          groupIt: 3,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 4,
+          shape: 3,
+          groupIt: 3,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 4,
+          shape: 4,
+          groupIt: 12,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A9',
+        element: {
+          asset: -1,
+          layer: 5,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A8',
+        element: {
+          asset: -1,
+          layer: 6,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A7',
+        element: {
+          asset: -1,
+          layer: 7,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A6',
+        element: {
+          asset: -1,
+          layer: 8,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A5',
+        element: {
+          asset: -1,
+          layer: 9,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'okay',
+        element: {
+          asset: -1,
+          layer: 10,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 10,
+          shape: 1,
+          groupIt: 3,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 10,
+          shape: 5,
+          groupIt: 4,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 10,
+          shape: 6,
+          groupIt: 4,
+        },
+      },
+      {
+        message: '形状图层的 Merge Paths， 在 iOS 和 Web 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'Web',
+          'iOS',
+        ],
+        rule: 'incompatible_merge_paths',
+        name: '合并路径 1',
+        element: {
+          asset: -1,
+          layer: 10,
+          shape: 7,
+          groupIt: 2,
+        },
+      },
+      {
+        message: '图层存在 “效果” 滤镜，影响渲染性能',
+        type: 'info',
+        rule: 'info_layer_effects',
+        name: 'shadow',
+        element: {
+          asset: -1,
+          layer: 11,
+        },
+      },
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: 'A10',
+        element: {
+          asset: -1,
+          layer: 12,
+        },
+      },
+    ]);
   });
 });
