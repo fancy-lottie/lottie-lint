15c6cc9ebb5d85ef43cf955afac622117121b666
王超 <sbxdwz@qq.com>
2019-08-28 15:12:20 +0800

    feat: 增加5.0.0+的判断细化，增加渐变的异常报警 (#11)
    * fix: 5.5.0规则细化
    * feat: 渐变问题报警
    * test: 补充5.5.0+判断的单元测试

--- test/incompatible.test.js
+++ test/incompatible.test.js
@@ -18,6 +18,61 @@ describe('version linter', function() {
       },
     ]);
   });
+  it('5.5.0+版本判断细化, 连带渐变描边、渐变填充规则', () => {
+    const lottieData = require('./case_data/gradient.json');
+    const reports = linter(lottieData).reports;
+    assert.deepStrictEqual(reports, [
+      {
+        message: '“形状” 如果可以转化成 “图片” 运行，性能可以更好',
+        type: 'info',
+        rule: 'info_layertype_shape',
+        name: '形状图层 1',
+        element: {
+          asset: -1,
+          layer: 0,
+        },
+      },
+      {
+        message: '渐变的绘制存在乱码异常',
+        type: 'warn',
+        rule: 'warn_gradient_warn',
+        name: '渐变填充 1',
+        element: {
+          asset: -1,
+          layer: 0,
+          shape: 0,
+          groupIt: 1,
+        },
+      },
+      {
+        message: '渐变的绘制存在乱码异常',
+        type: 'warn',
+        rule: 'warn_gradient_warn',
+        name: '渐变描边 1',
+        element: {
+          asset: -1,
+          layer: 0,
+          shape: 0,
+          groupIt: 2,
+        },
+      },
+      {
+        message: '形状图层的 Gradient Strokes，在 iOS 上不支持',
+        type: 'incompatible',
+        incompatible: [
+          'iOS',
+        ],
+        rule: 'incompatible_gradient_strokes',
+        name: '渐变描边 1',
+        element: {
+          asset: -1,
+          layer: 0,
+          shape: 0,
+          groupIt: 2,
+        },
+      },
+    ]);
+  });
   it('导出兼容版本', () => {
     const lottieData = require('./case_data/compatibility.json');
     const reports = linter(lottieData).reports;
