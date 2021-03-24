b515e566987f3a96bff04e116fe2fba4c7c9206b
天辙 <weiesky.wangc@alibaba-inc.com>
2021-03-24 16:45:59 +0800

    feat: 调整整体结构迁移到2.0，功能拆分成3层

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 
-import LottieLint from './LottieLint';
+import Standard from './core/standard';
+import Browser from './core/browser';
+import Other from './core/other';
+import Low from './core/low';
 
-function lottieLint(jsonData) {
-  const lottieLint = new LottieLint(jsonData);
-  return lottieLint.getResult();
-}
-
-lottieLint.LottieLint = LottieLint;
-export default lottieLint;
+export default {
+  // 基础的校验结构
+  standard(jsonData) {
+    return new Standard(jsonData).getResult();
+  },
+  // 支持客户端能力的附加检测图片的一些优化选项
+  browser(jsonData) {
+    const standard = new Standard(jsonData);
+    return new Browser(standard.getResult()).getResult();
+  },
+  // 带检测属性给滤过的标准执行判断
+  low(jsonData, config) {
+    const browser = this.lintBrowser(jsonData);
+    const other = new Other(jsonData).getResult();
+    return Low.getResult(jsonData, browser, other, config).getResult();
+  },
+};
